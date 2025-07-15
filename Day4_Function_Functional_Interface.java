a3c9603560a09903eb66d8f709aee6fff36980d6
your-name <shoaibakther812@gmail.com>
2025-07-16 01:49:51 +0530

    function functional inteface

---
+++ Day4_Function_Functional_Interface.java
@@ -0,0 +1,33 @@
+import java.util.function.Function;
+
+public class Day4_Function_Functional_Interface
+{
+	/*
+     =========================
+     Function Functional Interface
+     =========================
+
+    -> Function is predefined functional interface
+
+   -> Funcation interface having one abstract method i.e apply(T r)
+
+		interface Function<R,T>
+		{
+			R apply (T t);
+		}	
+
+   -> It takes input and it returns output
+  */
+
+	public static void main(String[] args) 
+	{
+
+		Function<String, Integer> f = (name) -> name.length();
+
+		System.out.println(f.apply("ashokit"));
+		System.out.println(f.apply("hyd"));
+		System.out.println(f.apply("sachin"));
+
+	}
+}
+
